1057ad02708d58016f6d239c82020550d801ccf5
Jeremy Rieunier <9092354+jeremyrieunier@users.noreply.github.com>
2025-07-24 22:37:04 +0200

    Modified layout and paying with css

---
+++ pages/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import '../app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+<EvidenceDefaultLayout {data} title="Jeremy Rieunier" githubRepo="https://github.com/jeremyrieunier/" xProfile="https://x.com/jeremyrieunier">
+	<slot slot="content" />
+</EvidenceDefaultLayout>
